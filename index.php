a42afdd59047cd0b644d4875a5e4683b4c337bfd
Kevin Stoop <kevinstoop9@gmail.com>
2021-11-23 16:39:37 +0100

    first things first

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Getting Started – Platform Generator</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/assets/css/style.min.css">
+</head>
+
+<body>
+  <main>
+    <div class="pg__doc">
+      <div class="pg__doc--title">Platform Generator</div>
+      <ul class="pg__doc--menu">
+        <li class="pg__doc--menu-item">
+          <span class="active">
+            Installatie
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+          </span>
+          <ul class="pg__doc--menu-item--list">
+            <li class="pg__doc--menu-item--list__item">
+              <a href="/">
+                Getting Started
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="pg__doc--menu-item">
+          <span class="">
+            Structuur
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+          </span>
+          
+        </li>
+      </ul>
+    </div>
+    <div class="pg__content" style="height:300vh">
+      <h1>Getting Started</h1>
+      <p>Deze pagina is een overzicht van de Platform Generator documentatie.</p>
+      <p>Platform generator</p>
+      <hr>
+      <p>
+        Test
+        <a href='#'>Linkiee</a>
+      </p>
+    </div>
+  </main>
+
+  <script src="/assets/js/main.ob.js"></script>
+</body>
+
+</html>
